722c4354a7a834cd6085e22577774bc5da824d23
Agata <twardowska123@gmail.com>
2018-08-16 17:52:31 +0200

    prezentacja zad 1 - try catch

--- src/Test.java
+++ src/Test.java
@@ -6,14 +6,19 @@ public class Test {
         System.out.println("Dostępne prędkości:");
         Speed[] values = Speed.values(); //pobranie wszystkich wartosci enuma
         for (Speed speed : values) {
-            System.out.println(speed + "(" + speed.translation + ")" );
+            System.out.println(speed + "(" + speed.translation + ")");
         }
         System.out.println("Wybierz predkość:");
         String x = scan.nextLine();
-        Speed speed = Speed.valueOf(x); //pobranie enuma za pomoca scannera
 
-        System.out.println("Wybrana wartość: " + speed);
-        System.out.println(speed.speedValue + "kmh");
+        try {
+            Speed speed = Speed.valueOf(x); //pobranie enuma za pomoca scannera
 
+            System.out.println("Wybrana wartość: " + speed);
+            System.out.println(speed.speedValue + "kmh");
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("Podana wartość jest nieprawidlowa");
+        }
     }
 }
